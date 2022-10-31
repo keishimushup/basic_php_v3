a9839c0656ac2af63dd871a45016d2788ba8838d
keishimushup <keishi908917@gmail.com>
2022-10-31 18:10:36 +0900

    humei

--- work/index.php
+++ work/index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title>U太郎のポートフォリオサイト</title>
+  <title>太郎のポートフォリオサイト</title>
   <link rel="icon" href="favicon.ico">
   <meta name="description" content="太郎のポートフォリオサイトです。">
   <link rel="stylesheet" href="css/styles.css">
